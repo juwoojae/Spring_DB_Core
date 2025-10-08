7be222d41a5c5cb909a5c5a3c1f2f7f17c4953d3
Juwoojae <juwqq1234@naver.com>
2025-10-09 01:33:13 +0900

    JDBC 개발 - 조회

--- src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
+++ src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
@@ -5,10 +5,17 @@ import hello.jdbc.domain.Member;
 import lombok.extern.slf4j.Slf4j;
 
 import java.sql.*;
+import java.util.NoSuchElementException;
 
 /**
  * PreparedStatement 는 Statement 의 자식 타입인데 ? 를 통한 파라메터 바인딩을 가능하게 해준다.
  * SQL injection 공격에 대바할수 있다
+ *
+ * ResultSet
+ * select 쿼리가 순서대로 들어간다
+ * 내부의 cursot 를 이동해서 다음 데이터를 조회할수 있다.
+ * rs.next() 이것을 호출하면 커서가 다음으로 이동한다. (최초의 커서는 데이터를 가지고 있지 않음 . 최초로 한번 호출해야한다)
+ * rs.getString("member_id) 현재 커서가 가리키고 있는 row 의 member_id 를 반환한다.
  */
 @Slf4j
 public class MemberRepositoryV0 {
@@ -34,6 +41,32 @@ public class MemberRepositoryV0 {
         }
     }
 
+    public Member findById(String memberId) throws SQLException{
+        String sql = "select * from member where member_id = ?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try{
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1,memberId); //해당 데이터베이스의 쿼리문의 ? 에 바인딩하기
+
+            rs = pstmt.executeQuery(); //데이터를 변경할때는 executeUpdate() 를 사용하지만, 조회할때는 executeQurey() 를 사용한다
+            if(rs.next()){ //resultSet 에서 찾은 Member 를 가지고 오는 과정 (커서가 존재해서 , 하나 다음칸으로 진행해야한다)
+                Member member = new Member();
+                member.setMemberId(rs.getString("member_id"));
+                member.setMoney(rs.getInt("money"));
+                return member;
+            }else{
+                throw new NoSuchElementException("member not found memberId=" + memberId);
+            }
+        }catch (SQLException e){
+            log.info("db error", e);
+            throw e;
+        }
+    }
     /**
      * 리소스 정리 메서드
      * 리소스를 정리할때에는 항상 역순으로 적용해주어야 한다
--- src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -1,12 +1,14 @@
 package hello.jdbc.repository;
 
 import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+@Slf4j
 class MemberRepositoryV0Test {
 
     MemberRepositoryV0 repository = new MemberRepositoryV0();
@@ -14,8 +16,15 @@ class MemberRepositoryV0Test {
     @Test
     void crud() throws SQLException{
         //save
-        Member member = new Member("memberV1", 10000);
+        Member member = new Member("memberV4", 10000);
         repository.save(member);
+
+        //findById
+        Member findMember = repository.findById(member.getMemberId());
+        log.info("findMember={}", findMember);
+        log.info("member == findMember {}", member == findMember); //참조를 같은것을해야 true 를 반환한다.
+        log.info("member equals findMember {}", member.equals(findMember)); //equals() 를 @Data lombok 에서 오버라이딩해서 선언해둠. 속성 끼리 비교해서, 모두 참이라면 true 를 반환한다
+        Assertions.assertThat(findMember).isEqualTo(member);
     }
 
 }
