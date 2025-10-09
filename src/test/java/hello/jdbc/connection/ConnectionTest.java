257529ed668532dcdc2bd9d22f59e73553f8d66f
Juwoojae <juwqq1234@naver.com>
2025-10-09 23:21:01 +0900

    DataSource예제2 - 커넥션 풀

--- src/test/java/hello/jdbc/connection/ConnectionTest.java
+++ src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -1,8 +1,10 @@
 package hello.jdbc.connection;
 
+import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.datasource.UserCredentialsDataSourceAdapter;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
@@ -32,6 +34,24 @@ public class ConnectionTest {
         useDataSource(dataSource);
     }
 
+    /**
+     * HikariDataSource 는 DataSource 인터페이스의 구현체이다
+     * 커넥션풀에서 커넥션을 생성하는 작업은 애플리케이션 실행속도와 독립적으로 별개의 스레드에서 실행된다.
+     * 그래서 테스트가 끝나도 계속 로그가 생김을 확인할수 있다
+     */
+    @Test
+    void dataSourceConnectionPool() throws SQLException, InterruptedException{
+        //커넥션 풀링 : HikariProxyConnection(Proxy) -> JdbcConnection(Target)
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        dataSource.setMaximumPoolSize(10); //커넥션 풀 사이즈를 10으로 지정
+        dataSource.setPoolName("MyPool"); //풀의 이름을 MyPool 이라고 지정
+
+        useDataSource(dataSource);
+        Thread.sleep(1000);
+    }
     private void useDataSource(DataSource dataSource) throws SQLException {
         Connection con1 = dataSource.getConnection();
         Connection con2 = dataSource.getConnection();
@@ -39,3 +59,4 @@ public class ConnectionTest {
         log.info("connection={}, class={}", con2, con2.getClass());
     }
 }
+
